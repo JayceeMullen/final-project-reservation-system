542dd660b68ca113d6ab25799785395cb8dbefb2
JayceeMullen <97761792+JayceeMullen@users.noreply.github.com>
2023-02-24 15:37:18 -0600

    Properly implement Reservations Interfaces

--- final-project-reservation-system/ReservationAPI/Interfaces/IReservationsController.cs
+++ final-project-reservation-system/ReservationAPI/Interfaces/IReservationsController.cs
@@ -7,4 +7,15 @@ public interface IReservationsController
 {
     Task<IActionResult> GetReservations();
     Task<IActionResult> CreateReservation(ReservationRequest reservationRequest);
+    
+    Task<IActionResult> GetReservationById(Guid id);
+    
+    Task<IActionResult> GetReservationsByCustomerId(Guid id);
+    
+    Task<IActionResult> GetReservationsByLocationId(Guid id);
+
+    Task<IActionResult> UpdateReservationById(Guid id, ReservationRequest reservationRequest);
+    
+    Task<IActionResult> DeleteReservation(Guid id);
+    
 }
--- final-project-reservation-system/ReservationAPI/Interfaces/IReservationsDao.cs
+++ final-project-reservation-system/ReservationAPI/Interfaces/IReservationsDao.cs
@@ -7,4 +7,14 @@ public interface IReservationsDao
     Task<IEnumerable<Reservation>> GetReservations();
     
     Task CreateReservation(ReservationRequest reservationRequest);
+    
+    Task DeleteReservation(Guid id);
+    
+    Task<Reservation> GetReservationsById(Guid id);
+    
+    Task<IEnumerable<Reservation>> GetReservationsByCustomerId(Guid id);
+    
+    Task<IEnumerable<Reservation>> GetReservationsByLocationId(Guid id);
+
+    Task UpdateReservation(Guid id, ReservationRequest reservationRequest);
 }

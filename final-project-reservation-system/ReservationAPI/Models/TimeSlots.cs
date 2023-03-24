22dedb8289aac31eecd8455164b8ed5928173682
SchmittZ3 <Zach.Schmitt@veteransunited.com>
2023-03-24 15:12:32 -0500

    3/24 .1

--- final-project-reservation-system/ReservationAPI/Models/TimeSlots.cs
+++ final-project-reservation-system/ReservationAPI/Models/TimeSlots.cs
@@ -4,11 +4,6 @@
     {
         public Guid LocationTimeSlotID { get; set; }
         public Guid LocationID { get; set; }
-        public string SlotStartTime { get;}
-
-        public TimeSlots(object slotStartTime)
-        {
-            SlotStartTime = slotStartTime.ToString();
-        }
+        public string SlotStartTime { get; set; }
     }
 }
